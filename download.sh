8e125ad84941330609e6ec80c1dbd27828a32919
hsuck <xmjj726716@gmail.com>
2022-10-13 15:01:45 +0800

    change download URL

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 mkdir -p ./ckpt/intent/
 mkdir -p ./ckpt/slot/
 
-INTENT_PT_URL="https://www.dropbox.com/s/snsd33kquauilqs/best_model.pt?dl=1"
+INTENT_PT_URL="https://www.dropbox.com/s/4lrgew5ruicywx6/best_model.pt?dl=1"
 INTENT_PT_PATH="./ckpt/intent/best_model.pt"
 wget -O ${INTENT_PT_PATH} ${INTENT_PT_URL}
 
-SLOT_PT_URL="https://www.dropbox.com/s/5emw2ajpvh0au9v/best_model.pt?dl=1"
+SLOT_PT_URL="https://www.dropbox.com/s/5v0rnyowqtmw0rq/best_model.pt?dl=1"
 SLOT_PT_PATH="./ckpt/slot/best_model.pt"
 wget -O ${SLOT_PT_PATH} ${SLOT_PT_URL}
