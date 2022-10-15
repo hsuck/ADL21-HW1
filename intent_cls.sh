de0629e56598cb9d532a8f90e7077955e451ddad
hsuck <xmjj726716@gmail.com>
2022-10-15 18:34:47 +0800

    modify argument passed

--- intent_cls.sh
+++ intent_cls.sh
@@ -1,3 +1,3 @@
 # "${1}" is the first argument passed to the script
 # "${2}" is the second argument passed to the script
-python3 test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/best_model.pt --pred_file "${2}" --dropout 0.2 --num_layers 3 --hidden_size 512
+python3 test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/GRU_best_model.pt --pred_file "${2}" --dropout 0.2 --num_layers 2 --hidden_size 512 --model GRU
